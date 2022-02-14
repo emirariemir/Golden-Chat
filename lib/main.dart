d5d7fe1457eef55b38fe208fc44e34f54553cd8a
Emir Arı <emirariomatic@gmail.com>
2022-02-14 15:35:07 +0300

    Add status bar color specification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:au_chat/screens/chat_screen.dart';
 import 'package:au_chat/screens/login_screen.dart';
 import 'package:au_chat/screens/registration_screen.dart';
+import 'package:flutter/services.dart';
 import 'screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,11 @@ class AuChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       initialRoute: 'welcome-screen',
       routes: {
